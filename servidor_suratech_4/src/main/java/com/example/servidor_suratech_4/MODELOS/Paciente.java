aaa94fcdc8c08aa2bd2ee2af44f0ad7ce0cc2e25
Lauragarciond <162141657+Lauragarciond@users.noreply.github.com>
2024-09-17 15:03:18 -0500

    sura4

---
+++ servidor_suratech_4/src/main/java/com/example/servidor_suratech_4/MODELOS/Paciente.java
@@ -0,0 +1,19 @@
+package com.example.servidor_suratech_4.MODELOS;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+   private Integer id;
+    private String nombres;
+    private Integer edad;
+    private  LocalDate fechaNacimiento;
+    private  String telefono;
+    private Integer copago;
+    private  String email;
+    private  String direccipn;
+    private  String ips;
+    private   String medicoFamilia;
+
+
+}
